c6d162b62edb51baedb6944dd43d99f852516b81
AleenaKhan673 <aleena.khan4@ontariotechu.net>
2024-11-24 23:50:33 -0500

    update index

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+/* GET index page. */
+router.get('/', function(req, res, next) {
+  res.render('home', { title: 'Home' });
+});
 /* GET home page. */
 router.get('/home', function(req, res, next) {
   res.render('home', { title: 'Home' });
